1a0c83f719c4e45adc2da3a9d2f780c6e3be79f1
shirish1995 <shirish1995@gmail.com>
2024-09-21 00:54:02 +0530

    na

--- 326.power-of-three.py
+++ 326.power-of-three.py
@@ -9,23 +9,15 @@ class Solution:
     def isPowerOfThree(self, n: int) -> bool:
         def power_of_three(k):
             if k == 1:
-                return True
-                
+                return True    
             if(k%3==0):
-
-                # result = k // 3
-                # print(result)
-                # print("True")
-                
                 return power_of_three(k//3)                
             else:
                 return False
-            
-        
         if n >0:
             return power_of_three(n)
-        else:
-            return False
+        else :
+            return Falses
             
 # @lc code=end
 
