808fd6f3a4c35acd68f4b12457faea81727e532b
shirish1995 <shirish1995@gmail.com>
2024-09-21 00:49:10 +0530

    power_of_three_solution

---
+++ 326.power-of-three.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode id=326 lang=python3
+#
+# [326] Power of Three
+#
+
+# @lc code=start
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        def power_of_three(k):
+            if k == 1:
+                return True
+                
+            if(k%3==0):
+
+                # result = k // 3
+                # print(result)
+                # print("True")
+                
+                return power_of_three(k//3)                
+            else:
+                return False
+            
+        
+        if n >0:
+            return power_of_three(n)
+        else:
+            return False
+            
+# @lc code=end
+
