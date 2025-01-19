1e23c9308e1a48a8786fd572834bbfff4f020030
shirish1995 <shirish1995@gmail.com>
2025-01-19 12:01:31 +0530

    no change

--- 48.rotate-image.py
+++ 48.rotate-image.py
@@ -18,11 +18,10 @@ class Solution:
             for j in range(i + 1, n):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-        
-
         # Reverse each row
         for i in range(n):
             matrix[i].reverse()
+            
         
 
         
