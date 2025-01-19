36ac84517588dde4da292fad932700d5465cb8f4
shirish1995 <shirish1995@gmail.com>
2025-01-19 14:29:25 +0530

    Q 832: flipping an image

--- 832.flipping-an-image.py
+++ 832.flipping-an-image.py
@@ -7,6 +7,24 @@
 # @lc code=start
 class Solution:
     def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
-        
+        result =[]
+        for inner_list in image:
+            n= len(inner_list)
+            inner_list.reverse() # printing increases the runtim and gives bad results
+            
+            # # this commented section below is for inverting the numbers inplace for image
+            # for idx in range(n):
+            #     inner_list[idx]= int(not inner_list[idx])
+
+            ## another method for solving the inversion 
+            # for idx in range(n):
+            #     inner_list[idx]= int(not inner_list[idx])
+            # return image
+
+            result.append([x ^ 1 for x in inner_list])
+
+
+
+        return result
 # @lc code=end
 
