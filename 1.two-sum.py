26b83a3e2a8c44e9b12d4b5e7a8e49b71b7721b0
srish <92784484+shirish1995@users.noreply.github.com>
2025-02-08 13:06:16 +0530

    redone: Q1: 2 Sum: using Hash Maps

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -7,11 +7,20 @@
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        length_of_list = len (nums)
-        for index in range(length_of_list):
-            for temp_index in range(index+1,length_of_list):
-                if (nums[index] + nums[temp_index]) == target:
-                    return [index,temp_index]
+        hashmap= {}
+        for i in range(len(nums)):
+            complement = target- nums[i]
+            if complement in hashmap :
+                print(i,hashmap[complement])
+                return [i,hashmap[complement]]
+
+            hashmap[nums[i]]= i 
+        return[]
+        # length_of_list = len (nums)
+        # for index in range(length_of_list):
+        #     for temp_index in range(index+1,length_of_list):
+        #         if (nums[index] + nums[temp_index]) == target:
+        #             return [index,temp_index]
             
             
 
